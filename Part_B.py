df661a7490d435328bbf61569b33ea9d011db3db
Carlos <carlosv2@illinois.edu>
2019-03-17 20:42:13 -0500

    Finito

--- Part_B.py
+++ Part_B.py
@@ -1,36 +1,56 @@
 import csv
-import matplotlib.pyplot as plt
+#Exercise1
+m = 4
+p = .5
+Exercise1 = p*((3+m*p)/(4+m))*((1+m*p)/(3+m))*((2+m*p)/(3+m))*((1+m*p)/(3+m))
+print("P(Delay|SEA,ATL,Southwest,Good) = " + str(Exercise1))
+print()
+#Exercise2
 inFileName = "FlightDelay.csv"
-inFile = open(inFileName,'r')
-preList = csv.reader(inFile,delimiter = ',')
+inFile = open(inFileName, 'r')
+preList = csv.reader(inFile, delimiter=',')
 next(preList)
-flightDict={}
+type(preList)
 flightList = []
 for line in preList:
     flightList.append(line)
-    #if line[0] not in flightDict:
-        #flightDict.update({line[0]:0.0})
-#for key in flightDict:
-    #for line in flightList:
-        #if key == line[0]:
-            #flightDict[key] += int(line[3])
-            #flightDict[key] += int(line[4])
 for line in flightList:
     line.append(int(line[3]) + int(line[4]))
     if line[5] < -15:
         line.append("Y")
-    else :
+    else:
         line.append("N")
-for a in flightList:
-    print(a)
+yes = 0
+for line in flightList:
+    if 'Y' in line:
+        yes += 1
+no = len(flightList) - yes
 
-def f(aLine):
-    yes = 0
-    no = 0
-    set = []
-    for line in flightList:
-        if line[0] == aLine:
-            set.append(line)
-    #sublist section works
 
-f('AA')
+#Exercise 3
+def NaiveBayes(airline, origin, destination):
+    arguments = [airline, origin, destination]
+    answer = [0.5, 0.5]
+    for element in arguments:
+        tempList = []
+        tempYes = 0
+        tempNo = 0
+        for line in flightList:
+            if element in line:
+                tempList.append(line)
+        for line in tempList:
+            if line[6] == 'Y':
+                tempYes += 1
+        tempNo = len(tempList) - tempYes
+        answer[0] *= (tempYes+m*p)/(yes + m)
+        answer[1] *= (tempNo+m*p)/(no + m)
+    print("P(Delay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[0]))
+    print("P(Notdelay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[1]))
+    print()
+
+
+NaiveBayes('AA', 'JFK', 'LAS')
+NaiveBayes('B6', 'JFK', 'LAS')
+NaiveBayes('VX', 'SFO', 'ORD')
+NaiveBayes('WN', 'SFO', 'ORD')
+
