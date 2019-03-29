6d791b5fc6980ce95680b8e89b6f217be99c9d18
Carlos <carlosv2@illinois.edu>
2019-03-29 04:43:30 -0500

    Cleaned up and commented more on how the Naive-Bayes function works

--- Part_B.py
+++ Part_B.py
@@ -20,7 +20,6 @@ inFileName = "FlightDelay.csv"
 inFile = open(inFileName, 'r')
 preList = csv.reader(inFile, delimiter=',')
 next(preList)
-type(preList)
 flightList = []
 for line in preList:
     flightList.append(line)
@@ -30,38 +29,42 @@ for line in flightList:
         line.append("Y")
     else:
         line.append("N")
-yes = 0
+
+yes = 0 # Computing total Y and N in the flightList
 for line in flightList:
     if 'Y' in line:
         yes += 1
 no = len(flightList) - yes
 
-
 #Exercise 3
 def NaiveBayes(airline, origin, destination):
     arguments = [airline, origin, destination] # Setting up a list of our arguments to iterate through
-    answer = [0.5, 0.5]
+    answer = [0.5, 0.5] # Initializing an answer list with two constants [P(Y),P(N)]
     probList=[]
-    for i in range(3):
+    for i in range(3): # fills in the list of probabilities for each argument
         counterList=[]
         for line in flightList:
             counterList.append(line[i])
         probList.append(1/len(list(dict.fromkeys(counterList))))
-    for element in arguments:
-        tempList = []
+    for element in arguments: # Iterates through each argument and evaluates their P(xi|Ck)
+        tempList = [] # Lets us know how many of that argument exist in the list and iterate through
         tempYes = 0
         tempNo = 0
-        for line in flightList:
+        for line in flightList: # Appends desired rows into a temporary list, fails if arguments appear in other columns
             if element in line:
                 tempList.append(line)
-        for line in tempList:
+        for line in tempList: # Determines how many in our temporary list have a Y and how many have an N
             if line[6] == 'Y':
                 tempYes += 1
         tempNo = len(tempList) - tempYes
         answer[0] *= (tempYes+m*(probList[arguments.index(element)]))/(yes + m)
         answer[1] *= (tempNo+m*(probList[arguments.index(element)]))/(no + m)
     print("P(Delay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[0]))
-    print("P(Notdelay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[1]))
+    print("P(NotDelay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[1]))
+    if answer[0] > answer[1]:
+        print("The flight will be delayed")
+    else:
+        print("The flight will not be delayed")
     print()
 
 
