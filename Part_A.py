494c14e0e23c16c5c0b9d4cd88ef35ace04fe4e7
Carlos <carlosv2@illinois.edu>
2019-03-28 19:41:34 -0500

    Final

--- Part_A.py
+++ Part_A.py
@@ -2,50 +2,51 @@ import csv
 import matplotlib.pyplot as plt
 inFileName = "FlightTime.csv"
 outPutFile = "ans"
-inFile = open(inFileName,'r')
+inFile = open(inFileName, 'r')
 outFile = open(outPutFile, 'w')
-a = csv.reader(inFile,delimiter = ',')
+a = csv.reader(inFile, delimiter=',')
 next(a)
 
 newList = []
 for line in a:
-    if int (line[9]) >= 230:
+    if int(line[9]) >= 230:
         newList.append(line)   #Number 1
 
-dataCount=len(newList)
+dataCount = len(newList)
 print("Total Number of Entries is " + str(dataCount))    #Number 2
 
 def TFT(d, lArr, lDes):
     return .117*d + 0.517*(lArr-lDes)+20
-targetFlightTime = TFT(1741.16,-87.90,-118.41)  #Number 3
+
+targetFlightTime = TFT(1741.16, -87.90, -118.41)  #Number 3
 for row in newList:
     row.append(float(targetFlightTime) + ((int(row[6])+int(row[8]))/2))
 print(newList)  #Number 4
 
-car=[]
+car = []
 for row in newList:
     if row[1] not in car:
         car.append(row[1])
 ansDict = {}
 for name in car:
-    ansDict.update({name:0.0})
+    ansDict.update({name: 0.0})
 for key in ansDict:
-    tempList=[0,0]
-    count=0.0
+    tempList = [0, 0]
+    count = 0.0
     for row in newList:
         if key == row[1]:
-            tempList[0] += float (row[9])
-            tempList[1] += float (row[10])
-            count+=1.0
-        if count > 0 :
+            tempList[0] += float(row[9])
+            tempList[1] += float(row[10])
+            count += 1.0
+        if count > 0:
             ans = (tempList[0]-tempList[1])/count
     ansDict[key] += ans
 print(ansDict) #Number 5
 print(min(ansDict.values())) #Number 5
 
-x=list(ansDict.keys())
-y=list(ansDict.values())
-plt.bar(x,y)
+x = list(ansDict.keys())
+y = list(ansDict.values())
+plt.bar(x, y)
 plt.title("Time Added in Airline Flights")
 plt.xlabel("Airline")
 
--- Part_B.py
+++ Part_B.py
@@ -1,10 +1,20 @@
 import csv
 #Exercise1
 m = 4
-p = .5
-Exercise1 = p*((3+m*p)/(4+m))*((1+m*p)/(3+m))*((2+m*p)/(3+m))*((1+m*p)/(3+m))
-print("P(Delay|SEA,ATL,Southwest,Good) = " + str(Exercise1))
+# Naive-Bayes Equation repreated product of ((nHat + m*p)/(n + m)) = P(xi|Ck)
+Exercise1Y = (.5)*((3+m*(1/2))/(4+m))*((1+m*(1/2))/(3+m))*((2+m*(1/3))/(3+m))*((1+m*(1/2))/(3+m)) #Calculating probability of a delay
+Exercise1N = (.5)*((1+m*(1/2))/(4+m))*((2+m*(1/2))/(3+m))*((1+m*(1/3))/(3+m))*((2+m*(1/2))/(3+m)) #Calculating probability of no delay
+print("Exercise 1")
+print((Exercise1Y,Exercise1N))
+if Exercise1Y > Exercise1N:
+    print("The flight will be delayed")
+else:
+    print("The flight will not be delayed")
 print()
+print()
+print("Exercise 3")
+
+
 #Exercise2
 inFileName = "FlightDelay.csv"
 inFile = open(inFileName, 'r')
@@ -16,7 +26,7 @@ for line in preList:
     flightList.append(line)
 for line in flightList:
     line.append(int(line[3]) + int(line[4]))
-    if line[5] < -15:
+    if line[5] > 15:
         line.append("Y")
     else:
         line.append("N")
@@ -29,8 +39,14 @@ no = len(flightList) - yes
 
 #Exercise 3
 def NaiveBayes(airline, origin, destination):
-    arguments = [airline, origin, destination]
+    arguments = [airline, origin, destination] # Setting up a list of our arguments to iterate through
     answer = [0.5, 0.5]
+    probList=[]
+    for i in range(3):
+        counterList=[]
+        for line in flightList:
+            counterList.append(line[i])
+        probList.append(1/len(list(dict.fromkeys(counterList))))
     for element in arguments:
         tempList = []
         tempYes = 0
@@ -42,8 +58,8 @@ def NaiveBayes(airline, origin, destination):
             if line[6] == 'Y':
                 tempYes += 1
         tempNo = len(tempList) - tempYes
-        answer[0] *= (tempYes+m*p)/(yes + m)
-        answer[1] *= (tempNo+m*p)/(no + m)
+        answer[0] *= (tempYes+m*(probList[arguments.index(element)]))/(yes + m)
+        answer[1] *= (tempNo+m*(probList[arguments.index(element)]))/(no + m)
     print("P(Delay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[0]))
     print("P(Notdelay|" + origin + ', ' + destination + ', ' + airline + ') = ' + str(answer[1]))
     print()
